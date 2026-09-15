4d01a53ab51c3c3478636b3cb90d893cd2c96cf7
ThorGameDev <Andrew.Thor.Allen@Gmail.com>
2026-09-15 11:06:18 -0700

    Display Comment Threads from JS

--- frontend/scripts/comments.ts
+++ frontend/scripts/comments.ts
@@ -1,12 +1,36 @@
 import { err } from "./lib/utils.ts"
 
-async function appendReplies(targetElement: HTMLElement) {
-    const response = await fetch("/api/blog/getReplies?lang=en&commentId=4");
-    // TODO: Error handling
+const langCode = document.getElementsByTagName("html")[0].getAttribute("lang")
+
+async function appendReplies(targetElement: HTMLElement, commentId: string): Promise<void> {
+    // unhide hidden comments if they exist
+    const preloaded = targetElement.querySelectorAll(":scope > .comment")
+    if (preloaded.length > 0) {
+        for (let i = 0; i < preloaded.length; i++) {
+            preloaded[i].setAttribute('class', 'comment')
+        }
+        return
+    }
+
+    // Get replies from backend
+    const response = await fetch(`/api/blog/getReplies?lang=${langCode}&commentId=${commentId}`);
+    if (!response.ok) {
+        err("Could not get replies from backend")
+    }
     const data = await response.text();
-    // there has to be better ways to do this
-    targetElement.innerHTML += data;
-    // TODO: reRoute each generated reply
+    targetElement.insertAdjacentHTML('beforeend', data);
+
+    const comments = targetElement.querySelectorAll(":scope > .comment")
+    for (let i = 0; i < comments.length; i++) {
+        reRouteComment(comments[i] as HTMLElement);
+    }
+}
+
+async function hideReplies(targetElement: HTMLElement): Promise<void> {
+    const comments = targetElement.querySelectorAll(":scope > .comment")
+    for (let i = 0; i < comments.length; i++) {
+        comments[i].setAttribute('class', 'comment hidden')
+    }
 }
 
 function reRouteComment(comment: HTMLElement): void {
@@ -20,10 +44,30 @@ function reRouteComment(comment: HTMLElement): void {
     }
     const commentExpand = comment.getElementsByClassName("commentExpand")[0]
     if (commentExpand != null) {
-        commentExpand.addEventListener('click', function (e) {
+        // It's probably better to use a boolean to toggle states, but for now, the oneshot loop works really well
+        commentExpand.addEventListener('click', function handler(e) {
+            // Prevent the standard no-js behavior
             e.preventDefault();
-            appendReplies(comment);
-        })
+
+            // Get commentId
+            const targLink = commentExpand.getAttribute("href") ?? err("No link attached to Comment Expand Link");
+            const params = new URL(targLink, window.location.origin).searchParams;
+            const commentId = params.get("commentId") ?? err("Comment link had no commentId")
+
+            // Add reply list to the end of the comment
+            appendReplies(comment, commentId);
+
+            // Replace the link functionality with a "Hide replies" function
+            commentExpand.addEventListener('click', function (e) {
+                // Prevent the standard no-js behavior
+                e.preventDefault();
+
+                hideReplies(comment);
+
+                // Once the replies are hidden, re-attach the default "show" behavior
+                commentExpand.addEventListener('click', handler, { once: true })
+            }, { once: true })
+        }, { once: true })
     }
 }
 
